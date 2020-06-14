e43c30a2a584c578261278f1e620353f8511a4c9
ZhengAu <598359997@qq.com>
2020-06-14 17:33:23 +0800

    fix getqualifiedfunctionname

--- GlobalFunc.js
+++ GlobalFunc.js
@@ -32,6 +32,68 @@ class GlobalFunc {
 		return className ? className : 'anonymous';
 	}
 
+	/**
+	 * 带有完整自定义的类名，自定义类名+函数名
+	 * @param {Function} func 
+	 * @param {any} obj 
+	 */
+	getQualifiedFunctionName2(func, obj) {
+		let prefixStr = '';
+		if (obj) {
+			let prefix = this.getQualifiedClassName(obj);
+			if (prefix) {
+				prefixStr = prefix + '.';
+			}
+		}
+
+		if (!func || typeof func !== 'function') {
+			console.error(`传入参数不对 --- ${func}`);
+			return prefixStr + typeof func;
+		}
+		if (func.name) {
+			return prefixStr + func.name;
+		}
+
+		let prototype = func.prototype ? func.prototype : Object.getPrototypeOf(func);
+		if (prototype && prototype.constructor.name) {
+			return prefixStr + prototype.constructor.name;
+		}
+
+		let constructorString = prototype.constructor.toString().trim();
+		let index = constructorString.indexOf('(');
+		let className = constructorString.substring(9, index);
+
+		Object.defineProperty(prototype, '__class__', {
+			value: className,
+			enumerable: false,
+			writable: true,
+		});
+		return className ? prefixStr + className : prefixStr + 'anonymous';
+	}
+
+	/**
+	 * 获取自定义类的完整类名
+	 * @param {any} obj 
+	 */
+	getQualifiedClassName(obj) {
+		if (!obj || !obj.toString()) {
+			return typeof obj;
+		}
+
+		let res = '';
+		let str = obj.toString().trim();
+		// console.log(str.toString(), str.indexOf('class'), str.indexOf('function'));
+
+		if (str.indexOf('class') > -1) {
+			res = str.substring(0, str.indexOf('{')).replace('class', '').trim();
+		} else if (str.indexOf('function') > -1) {
+			res = str.substring(0, str.indexOf('(')).replace('function', '').trim();
+		} else {
+			res = typeof obj;
+		}
+		return res;
+	}
+
 }
 
 export const globalFunc = new GlobalFunc();
